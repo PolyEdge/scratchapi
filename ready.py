780fd84adfdf7b472fa0ecea4cb64215540699b5
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-12-12 11:24:20 -0700

    Update and rename ready.py to setup.py

--- ready.py
+++
@@ -1,7 +0,0 @@
-import pip
-try:
-  import requests
-except:
-  pip.main(['install', 'requests'])
-import shutil
-shutil.copyfile('ScratchAPI', 'C:\\Python34\\Lib\\ScratchAPI.py')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='scratchapi',
+      version='1.0',
+      description='Scratch API Script',
+      author='Dylan Beswick',
+      author_email='djbeswick64@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['requests>=2.8.1'],
+     )
